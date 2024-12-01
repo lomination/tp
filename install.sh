130bafdefb6432d54e3c08b580a5cec62422fc20
lomination <lomination@rejna.org>
2024-12-01 22:23:44 +0100

    Use echo -e instead of printf

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install() {
     cd "afs/.bin/"
     curl "https://raw.githubusercontent.com/lomination/tp/refs/heads/main/tp" > tp
     chmod +x tp
-    printf "\033[32mInstallation succeeded!\033[32m Make sure to restart you shell before trying.\n"
+    echo -e "\033[32mInstallation succeeded!\033[32m Make sure to restart you shell before trying."
 }
 
 cd
@@ -12,7 +12,7 @@ if [[ -e "afs/.bin" ]]; then
     if [[ -d "afs/.bin" ]]; then
         install
     else
-        printf "\033[31mERROR\033[0m: $(pwd)/.bin already exists but is not a directory. Consider removing the file, using the manual installation or asking for help to @lomination on Discord.\n"
+        echo -e "\033[31mERROR\033[0m: $(pwd)/.bin already exists but is not a directory. Consider removing the file, using the manual installation or asking for help to @lomination on Discord."
         exit 1
     fi
 else
